c62953f56a5429c45ba333d548b8648b884a20aa
alextsil <alex19924@gmail.com>
2016-12-16 17:43:21 +0200

    Finished word object class

--- src/main/scala/pojo/SenticNet4Word.scala
+++ src/main/scala/pojo/SenticNet4Word.scala
@@ -1,10 +1,31 @@
 package pojo
 
-//Todo : enum gia to polarity
-
-class SenticNet4Word(text: String, pleasantness: Float, attention: Float, sensitivity: Float, aptitude: Float,
-                     moodTags: List[String], polarity: Polarity) {
-
+object Polarity extends Enumeration {
+  val Positive, Negative = Value
 }
 
+class SenticNet4Word(val text: String, val pleasantness: Float, val attention: Float,
+                     val sensitivity: Float, val aptitude: Float, val moodTags: List[String],
+                     val polarity: Polarity.Value, val intensity: Float) {
+  //
 
+  //Prints all fields and field values
+  override def toString = s"SenticNet4Word(text=$text, pleasantness=$pleasantness, attention=$attention," +
+    s" sensitivity=$sensitivity, aptitude=$aptitude, moodTags=$moodTags, polarity=$polarity, intensity=$intensity)"
+
+
+  def canEqual(other: Any): Boolean = other.isInstanceOf[SenticNet4Word]
+
+  //Objects are equal when text fields match
+  override def equals(other: Any): Boolean = other match {
+    case that: SenticNet4Word =>
+      (that canEqual this) &&
+        text == that.text
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(text)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
