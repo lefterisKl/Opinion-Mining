ed930c954bf50f1c29774b649d33f7f20aaf2fc1
lefterisKl <lefteris.kl@hotmail.com>
2017-01-01 23:13:13 +0200

    added chain of review preprocessing

--- build.sbt
+++ build.sbt
@@ -5,6 +5,8 @@ lazy val root = (project in file(".")).
     scalaVersion := "2.11.8"
 	
   )
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.1"
 
-
+libraryDependencies ++= Seq(
+	"org.apache.spark" %% "spark-core" % "2.0.1",
+	"org.apache.spark"  %% "spark-mllib" % "2.0.1"
+)
--- src/main/scala/Opinion-Mining.scala
+++ src/main/scala/Opinion-Mining.scala
@@ -4,6 +4,9 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.spark.mllib.linalg.SparseVector
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+
 import dictionarybuilder.DictionaryBuilder
 
 
@@ -15,19 +18,31 @@ object OpinionMining {
 	
 	/*example
 	input: RDD["One    two. three, four   five-six"]
-	output: RDD["one two three four five six"]
+	output: RDD[Array("one", "two", "three", "four", "five", "six")]
 	*/
-	def processReviews(reviews: org.apache.spark.rdd.RDD[String]):org.apache.spark.rdd.RDD[String] = 
-	{ reviews.map(x=>x.replaceAll("""[\p{Punct}]"""," ").replaceAll("""\s+"""," ").toLowerCase) }
+	def processReviews(reviews: org.apache.spark.rdd.RDD[String]):org.apache.spark.rdd.RDD[Array[String]] = 
+	{ reviews.map(x=>x.replaceAll("""[\p{Punct}]"""," ").replaceAll("""\s+"""," ").toLowerCase.split(" ")) }
 
 
 	/*example
-	input: RDD["alpha alpha alpha beta beta", "word1 word2 word2 word2 word3 word4"]
+	input: RDD[Array("alpha", "alpha", "alpha", "beta", beta"), Array("word1", "word2", "word2", "word2", "word3", "word4"]
 	output: RDD[Array(("alpha",3),("beta",2)), Array(("word1",1),("word2",3),("word3",1),("word4",1)]
 	*/
-	def getTermFrequencies(reviews: org.apache.spark.rdd.RDD[String]):org.apache.spark.rdd.RDD[Array[(String, Int)]] = 
-	{ reviews.map(x=> x.split(" ").groupBy(x=>x).mapValues(_.size).toArray) }
+	def getTermFrequencies(reviews: org.apache.spark.rdd.RDD[Array[String]]):org.apache.spark.rdd.RDD[Array[(String, Int)]] = 
+	{ reviews.map(x=> x.groupBy(x=>x).mapValues(_.size).toArray) }
 
+	def filterWithDictionary(reviews: org.apache.spark.rdd.RDD[Array[String]],dictionary: scala.collection.Map[String,String])
+	:org.apache.spark.rdd.RDD[Array[String]] 
+	={reviews.map(termList => termList.map( term => dictionary.getOrElse(term,"NULL")).filter(term=> term!="NULL")) }
+	
+	def vectorize(reviewTF: Array[(String,Int)], idMap: scala.collection.immutable.Map[String,Int], numTerms: Int ): org.apache.spark.mllib.linalg.Vector =
+	{
+		 Vectors.sparse(numTerms, reviewTF.map( pair => (idMap(pair._1),pair._2.toDouble) ))
+	}
+
+	
+	//argument 1: path to project
+	//argument 2: path to data
 
 	def main(args: Array[String]) {
 
@@ -39,16 +54,34 @@ object OpinionMining {
 		val conf = new SparkConf().setAppName("Opinion-Mining")
 		val sc = new SparkContext(conf)
 		
-
-		val trainPos = sc.textFile("hdfs://localhost:9000/dataSubset/train/pos")
-		val trainNeg = sc.textFile("hdfs://localhost:9000/dataSubset/train/neg")
-
-		val trainPosFreq = getTermFrequencies( processReviews(trainPos))
-		val trainNegFreq = getTermFrequencies( processReviews(trainNeg))
 		
-		println(trainPosFreq.take(2).deep.mkString("\n\n"))
+		val trainPos = sc.textFile(args(0) + "/train/pos")
+		val trainNeg = sc.textFile(args(0) + "/train/neg")
+		val test = sc.textFile(args(0) +"/test")
+
+		val stemPairMap = sc.textFile(args(1)+"/stemPairsSorted.txt").map(x=>(x.split(" ")(0),x.split(" ")(1))).collectAsMap()
+		val termIdMap = stemPairMap.values.toSet.toArray.sorted.zipWithIndex.toMap
+
+		
+		val stemPairMapBC = sc.broadcast(stemPairMap)
+		val spm = stemPairMapBC.value
+
+		val termIdMapBC = sc.broadcast(termIdMap)
+		val tim = termIdMapBC.value
+		
+		val numTerms = tim.keys.size
+
+
+
+		val trainPosVectors = getTermFrequencies( filterWithDictionary( processReviews(trainPos), spm)).map(x=>vectorize(x,tim,numTerms))
+		val trainNegVectors = getTermFrequencies( filterWithDictionary( processReviews(trainNeg), spm)).map(x=>vectorize(x,tim,numTerms))
+
+
+		val testVectors = getTermFrequencies( filterWithDictionary( processReviews(test),spm)).map(x=>vectorize(x,tim,numTerms))
+
+		println(trainPosVectors.take(2).deep.mkString("\n\n"))
 		
 		println("SUCCESS")
-
+		
 	}
 }
