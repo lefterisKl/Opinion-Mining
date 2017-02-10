f905b4ce2212e852a443d0b984a453199d70ad14
lefterisKl <lefteris.kl@hotmail.com>
2017-02-10 09:08:51 +0200

    Make LabeledPoints / Add SVM

--- src/main/scala/Opinion-Mining.scala
+++ src/main/scala/Opinion-Mining.scala
@@ -7,7 +7,23 @@ import org.apache.log4j.Level
 import org.apache.spark.mllib.linalg.SparseVector
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
-import dictionarybuilder.DictionaryBuilder
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+import org.apache.spark.sql.types.StructType
+
+import org.apache.spark.sql.types.StructField
+
+
 
 
 
@@ -16,18 +32,12 @@ object OpinionMining {
 
 	
 	
-	/*example
-	input: RDD["One    two. three, four   five-six"]
-	output: RDD[Array("one", "two", "three", "four", "five", "six")]
-	*/
+	
 	def processReviews(reviews: org.apache.spark.rdd.RDD[String]):org.apache.spark.rdd.RDD[Array[String]] = 
 	{ reviews.map(x=>x.replaceAll("""[\p{Punct}]"""," ").replaceAll("""\s+"""," ").toLowerCase.split(" ")) }
 
 
-	/*example
-	input: RDD[Array("alpha", "alpha", "alpha", "beta", beta"), Array("word1", "word2", "word2", "word2", "word3", "word4"]
-	output: RDD[Array(("alpha",3),("beta",2)), Array(("word1",1),("word2",3),("word3",1),("word4",1)]
-	*/
+	
 	def getTermFrequencies(reviews: org.apache.spark.rdd.RDD[Array[String]]):org.apache.spark.rdd.RDD[Array[(String, Int)]] = 
 	{ reviews.map(x=> x.groupBy(x=>x).mapValues(_.size).toArray) }
 
@@ -40,12 +50,20 @@ object OpinionMining {
 		 Vectors.sparse(numTerms, reviewTF.map( pair => (idMap(pair._1),pair._2.toDouble) ))
 	}
 
+	def evaluateAccurasy(m:org.apache.spark.mllib.classification.ClassificationModel,test:org.apache.spark.rdd.RDD[LabeledPoint]):Double = {
+		val correct = test.map { point => val score = m.predict(point.features)
+		(score== point.label) }.filter(x=>(x==true)).count()
+		val numTest = test.count()
+		1.0*correct/numTest
+	}
+	
+
 	
 	//argument 1: path to project
 	//argument 2: path to data
 
 	def main(args: Array[String]) {
-
+		
 		Logger.getLogger("org").setLevel(Level.ERROR)
 		Logger.getLogger("akka").setLevel(Level.ERROR)
 		
@@ -55,9 +73,9 @@ object OpinionMining {
 		val sc = new SparkContext(conf)
 		
 		
-		val trainPos = sc.textFile(args(0) + "/train/pos")
-		val trainNeg = sc.textFile(args(0) + "/train/neg")
-		val test = sc.textFile(args(0) +"/test")
+		val trainPos = sc.textFile(args(0) + "/train/pos").repartition(4)
+		val trainNeg = sc.textFile(args(0) + "/train/neg").repartition(4)
+		val test = sc.textFile(args(0) +"/test",4)
 
 		val stemPairMap = sc.textFile(args(1)+"/stemPairsSorted.txt").map(x=>(x.split(" ")(0),x.split(" ")(1))).collectAsMap()
 		val termIdMap = stemPairMap.values.toSet.toArray.sorted.zipWithIndex.toMap
@@ -75,13 +93,25 @@ object OpinionMining {
 
 		val trainPosVectors = getTermFrequencies( filterWithDictionary( processReviews(trainPos), spm)).map(x=>vectorize(x,tim,numTerms))
 		val trainNegVectors = getTermFrequencies( filterWithDictionary( processReviews(trainNeg), spm)).map(x=>vectorize(x,tim,numTerms))
-
-
 		val testVectors = getTermFrequencies( filterWithDictionary( processReviews(test),spm)).map(x=>vectorize(x,tim,numTerms))
 
-		println(trainPosVectors.take(2).deep.mkString("\n\n"))
 		
+		val labeledVectors = trainPosVectors.map(x=>LabeledPoint(1.0,x)).union(trainNegVectors.map(x=>LabeledPoint(0.0,x))).persist(StorageLevel.MEMORY_AND_DISK)
+
+		val splits = labeledVectors.randomSplit(Array(0.5, 0.5), seed = 11L)
+		val training = splits(0).cache()
+		val testing = splits(1)
+
+
+		//SVM model
+		val numIterations = 100
+		val svm_model = SVMWithSGD.train(training, numIterations)
+		println(evaluateAccurasy(svm_model,testing))
+
+
+		//println(trainPosVectors.take(2).deep.mkString("\n\n"))
 		println("SUCCESS")
+		
 		
 	}
 }
